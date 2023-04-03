4d4db93130a6b56288de182b15ab5c99ae1d8780
leoferreiralima <36305985+leoferreiralima@users.noreply.github.com>
2023-04-03 07:53:37 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const graph = {
+  a: ['b', 'c'],
+  b: ['d'],
+  c: ['e'],
+  d: ['f'],
+  e: [],
+  f: [],
+};
+
+console.log('dephtFirst');
+dephtFirst(graph);
+
+console.log('breadhFirst');
+breadhFirst(graph);
+
+function breadhFirst(graph) {
+  const breadhFirstQueue = ['a'];
+
+  do {
+    const currentNode = breadhFirstQueue.shift();
+
+    console.log(currentNode);
+
+    breadhFirstQueue.push(...graph[currentNode]);
+  } while (breadhFirstQueue.length > 0);
+}
+
+function dephtFirst(graph) {
+  const dephtFirstStack = ['a'];
+
+  do {
+    const currentNode = dephtFirstStack.pop();
+
+    console.log(currentNode);
+
+    dephtFirstStack.push(...graph[currentNode]);
+  } while (dephtFirstStack.length > 0);
+}
